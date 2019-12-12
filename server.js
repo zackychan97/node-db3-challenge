9f3ab6b81d02965a0628710ca1e20003c5eb98a7
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-12 14:58:39 -0600

    mvp

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use('/', (req, res) => {
+    res.send({ message: "Server running..." })
+})
+
 module.exports = server;
